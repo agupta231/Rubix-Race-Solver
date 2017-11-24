40c84c6e95c235f99fdef264a2166d2e4436508c
Ankur Gupta <rukna1000@gmail.com>
2017-11-24 00:38:03 -0500

    Added the override method in the BFS

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class BreadthFirstSearch implements GraphSearchEngine {
+    @Override
+    public LinkedList<Node> shortestPath(Node node1) {
+        return null;
+    }
+}

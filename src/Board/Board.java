0d0aae5f4f20d1d99e562d590a3dbe986302b4cb
Ankur Gupta <rukna1000@gmail.com>
2017-12-19 19:58:57 -0500

    Added starting and end positions

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,7 +1,17 @@
 package Board;
 
 public class Board extends Node<BoardColor[][]> {
-    public static BoardColor[][] startingBoard;
+    public static BoardColor[][] startingBoard = new BoardColor[][]
+                   {{BoardColor.RED, BoardColor.GREEN, BoardColor.GREEN, BoardColor.WHITE, BoardColor.YELLOW},
+                    {BoardColor.RED, BoardColor.ORANGE, BoardColor.YELLOW, BoardColor.RED, BoardColor.BLANK},
+                    {BoardColor.GREEN, BoardColor.ORANGE, BoardColor.YELLOW, BoardColor.RED, BoardColor.ORANGE},
+                    {BoardColor.ORANGE, BoardColor.BLUE, BoardColor.BLUE, BoardColor.WHITE, BoardColor.BLUE},
+                    {BoardColor.YELLOW, BoardColor.GREEN, BoardColor.WHITE, BoardColor.BLUE, BoardColor.YELLOW}};
+
+    public static BoardColor[][] endingBoard = new BoardColor[][]
+                    {{BoardColor.ORANGE, BoardColor.YELLOW, BoardColor.WHITE},
+                    {BoardColor.ORANGE, BoardColor.YELLOW, BoardColor.RED},
+                    {BoardColor.BLUE, BoardColor.BLUE, BoardColor.WHITE}};
 
     public Board(BoardColor[][] state) {
         this.setData(state);

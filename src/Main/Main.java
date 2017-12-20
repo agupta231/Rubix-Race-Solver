51d916e2bffb5b56177d90ebc75347d98939aae5
Ankur Gupta <rukna1000@gmail.com>
2017-12-20 12:25:20 -0500

    Got some red, got some progress

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -22,21 +22,22 @@ public class Main {
                 {BoardColor.BLUE, BoardColor.BLUE, BoardColor.WHITE}};
 
 
-//        Board startingNode = new Board(startingBoard);
-//
-//        System.out.println("Here");
-//
-//        RubixRaceCombinationsGraph graph = new RubixRaceCombinationsGraph();
-//        graph.generateGraph(startingNode);
-//
-//        System.out.println("Here2");
-//
-//        BreadthFirstSearch searchEngine = new BreadthFirstSearch();
-//
-//        LinkedList<Board> optimalSolution = searchEngine.shortestPath(startingNode, endingBoard, graph);
-//
-//        System.out.println(optimalSolution);
+        Board startingNode = new Board(startingBoard);
+
+        System.out.println("Here");
+
+        RubixRaceCombinationsGraph graph = new RubixRaceCombinationsGraph();
+        graph.generateGraph(startingNode);
+
+        System.out.println("Here2");
+
+        BreadthFirstSearch searchEngine = new BreadthFirstSearch();
+
+        LinkedList<Board> optimalSolution = searchEngine.shortestPath(startingNode, endingBoard, graph);
+
+        System.out.println(optimalSolution);
 
         System.out.println(Arrays.deepHashCode(startingBoard) == Arrays.deepHashCode(startingBoard.clone()));
+        System.out.println(BoardColor.WHITE.equals(BoardColor.WHITE));
     }
 }
